0984121a2aa0a7d7fd9ab999330c852b79406877
Shirley suarez <shirley.startary@gmail.com>
2018-04-17 15:08:03 -0500

    haciendo pruebas de modificacion

--- src/App.js
+++ src/App.js
@@ -10,9 +10,6 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Petinder</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <p>Esto es una aplicacion de prueba</p>
       </div>
     );
