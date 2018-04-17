9711f6a6704dbeded0459f99c798c62fa9f2a05f
Shirley suarez <shirley.startary@gmail.com>
2018-04-17 14:59:52 -0500

    este es un commit de prueba

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Petinder</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Esto es una aplicacion de prueba</p>
       </div>
     );
   }
